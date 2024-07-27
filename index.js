8287758ed61a14dbac30d558712f770153fbb8c0
Ali <alirazahaider2@gmail.com>
2024-07-27 19:51:00 +0500

    Final Commit

--- index.js
+++ index.js
@@ -1,79 +1,14 @@
 var randomPlayer1 = Math.floor((Math.random() * 6) + 1);
+
+var imageSource1 = "images/dice" + randomPlayer1 + ".png";
+
 var randomPlayer2 = Math.floor((Math.random()*6)+1);
 
-function changeDice(randomNumber) {
-    
+var imageSource2 = "images/dice" + randomPlayer2 + ".png";
 
-    if (randomNumber === 1) {
-        var element = document.getElementsByClassName("img1");
-        element[0].setAttribute("src","/images/dice1.png");
-    }
+document.querySelectorAll("img")[0].setAttribute("src", imageSource1);
+document.querySelectorAll("img")[1].setAttribute("src", imageSource2);
 
-    else if (randomNumber === 2) {
-        var element = document.getElementsByClassName("img1");
-        element[0].setAttribute("src","/images/dice2.png");
-    }
-
-    else if (randomNumber === 3) {
-        var element = document.getElementsByClassName("img1");
-        element[0].setAttribute("src","/images/dice3.png");
-    }
-
-    else if (randomNumber === 4) {
-        var element = document.getElementsByClassName("img1");
-            element[0].setAttribute("src","/images/dice4.png");
-    }
-
-    else if (randomNumber === 5) {
-        var element = document.getElementsByClassName("img1");
-        element[0].setAttribute("src","/images/dice5.png");
-
-    }
-
-    else {
-        var element = document.getElementsByClassName("img1");
-        element[0].setAttribute("src","/images/dice6.png");
-}
-    
-}
-
-function changeDice2(randomNumber) {
-    
-
-    if (randomNumber === 1) {
-        var element = document.getElementsByClassName("img2");
-        element[0].setAttribute("src","/images/dice1.png");
-    }
-
-    else if (randomNumber === 2) {
-        var element = document.getElementsByClassName("img2");
-        element[0].setAttribute("src","/images/dice2.png");
-    }
-
-    else if (randomNumber === 3) {
-        var element = document.getElementsByClassName("img2");
-        element[0].setAttribute("src","/images/dice3.png");
-    }
-
-    else if (randomNumber === 4) {
-        var element = document.getElementsByClassName("img2");
-            element[0].setAttribute("src","/images/dice4.png");
-    }
-
-    else if (randomNumber === 5) {
-        var element = document.getElementsByClassName("img2");
-        element[0].setAttribute("src", "/images/dice5.png");
-
-    }
-
-    else {
-        var element = document.getElementsByClassName("img2");
-        element[0].setAttribute("src","/images/dice6.png");
-}
-    
-}
-changeDice(randomPlayer1);
-changeDice2(randomPlayer2);
 
 
 
